83f0cfee9c4710ac5af83cb8970bd37243b1b336
Shruti Agrawal <shruti98932@gmail.com>
2021-06-26 14:45:33 -0400

    Comments added

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,18 +3,22 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    // TODO
   },
 
   render: function() {
+    // Clear Message Feed
     MessagesView.$chats.html('');
+    // Iterate through all messages and append to DOM
     for (var message of Messages.result) {
+      // Provide defaults if no user input
       if (!message.username) {
         message.username = 'Anonymous';
       }
       if (!message.roomname) {
         message.roomname = 'Lobby';
       }
-
+      // Build chat info
       var messageInfo = {
         username: message.username,
         message: message.text,
@@ -25,6 +29,7 @@ var MessagesView = {
   },
 
   renderMessage: function(message) {
+    // Pass chat info to Underscore template in messageView, append to DOM
     var chatHtml = MessageView.render(message);
     MessagesView.$chats.append(chatHtml);
   },
